cbbf3a4ca5184864b47807d0183f976a3621d32a
christymaesumongod <christymaesumongod97@gmail.com>
2026-05-15 11:40:34 +0800

    Delete Photography Portfolio/nav.js

--- Photography Portfolio/nav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const navMenu = document.getElementById("navMenu");
-
-    hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-    });
-});
